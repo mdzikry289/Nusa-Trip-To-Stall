2cdcac2b66217cb2373878dcd72f23e93dc250b7
mdzikry289 <53846667+mdzikry289@users.noreply.github.com>
2021-11-01 12:18:09 +0700

    first commit

---
+++ Assets/Scripts/Coins.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coins : MonoBehaviour{
+    void OnCollisionEnter2D (Collision2D col){
+        if (col.gameObject.tag.Equals ("Player")){
+            print("coin diambil");
+            Destroy (gameObject);
+        }
+    }
+}
